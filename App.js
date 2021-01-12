b9a68e0eca88c74206763a90f3d7f9a0afab8383
aquiles <aquiles.viza@sansano.usm.cl>
2021-01-12 13:49:59 -0300

    agregado native-base, ejemplo base y funcional

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { Asset } from 'expo-asset';
+import AppLoading from 'expo-app-loading';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+    state = {
+        isReady: false,
+    };
+
+    render() {
+        if (!this.state.isReady) {
+            return (
+                <AppLoading
+                    startAsync={this._cacheResourcesAsync}
+                    onFinish={() => this.setState({ isReady: true })}
+                    onError={console.warn}
+                />
+            );
+        }
+
+        return (
+            <View style={{ flex: 1 }}>
+                <Image source={require('./assets/test_image.jpg')} />
+            </View>
+        );
+    }
+
+    async _cacheResourcesAsync() {
+        const images = [require('./assets/test_image.jpg')];
+
+        const cacheImages = images.map(image => {
+            return Asset.fromModule(image).downloadAsync();
+        });
+        return Promise.all(cacheImages);
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
