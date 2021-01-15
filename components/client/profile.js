d297e5123e09d6cd70e9a6e22c8df0dbf325c4a5
aquiles <aquiles.viza@sansano.usm.cl>
2021-01-15 15:41:08 -0300

    Falta arreglar el profile del cliente

--- components/client/profile.js
+++ components/client/profile.js
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Header, Button, Container, Text, Body, Left, Right, Icon, Content, Card, CardItem } from "native-base"
+import {
+    Header, Button, Container, Text, Body, Left,
+    Right, Icon, Content, Card, CardItem, Spinner
+} from "native-base"
 
-const ProfileClientScreen = ({ navigation }) => {
+import axios from 'axios'
+
+const ProfileClientScreen = ({ navigation, route }) => {
+    const [profile, setProfile] = useState()
+    const [loaded, setLoader] = useState(false)
+
+    useEffect(() => {
+        if (loaded) return;
+
+        const fetchClient = async () => {
+            const config = {
+                headers: { "access-token": route.params.jwt }
+            };
+
+            const res = await axios.get(
+                "https://proyecto-app-web-2020-2.herokuapp.com/users/" + route.params.id,
+                config)
+
+            console.log("Ya, entonces tenemos los datitos")
+
+            if (res.data.status == "OK") {
+                console.log(datos.data)
+                setProfile(res.data)
+                setLoader(true)
+            }
+
+        }
+        try {
+            fetchClient()
+        }
+        catch (e) {
+            console.log("Problema al cargar los datos del cliente: " + e)
+        }
+    })
+
+    if (!profile) {
+        return (
+            <Container>
+                <Header />
+                <Content>
+                    <Spinner color="blue" />
+                </Content>
+            </Container>
+        )
+    }
+
     return (
         <Container>
             <Header >
